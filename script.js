a3314600153895189736b71676d3092eb54c3263
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2024-12-18 21:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+let targetElement = document.getElementById("level");
+
+
+let level = 0;
+
+
+while (targetElement) {
+    level++;
+    targetElement = targetElement.parentElement;
+}
+
+
+alert("The level of the element is: " + level);
